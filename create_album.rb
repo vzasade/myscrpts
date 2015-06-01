46a80d531af9adf8b9f527903ff2fa885920330c
Artem Stemkovski <astemkov@gmail.com>
2015-05-31 19:35:08 -0700

    formatting in create_album.rb

--- create_album.rb
+++ create_album.rb
@@ -16,21 +16,21 @@ def getLastPathElem(path)
 end
 
 def setAlbumVar(album, key, value)
-    if value == nil then
-      puts key + " is not specified"
+  if value == nil then
+    puts key + " is not specified"
+    exit(0)
+  end
+
+  if album[key] == nil then
+    album[key] = value
+  else
+    if album[key] != value then
+      puts key + " [" + value + "] does not match [" + album[key] + "."
       exit(0)
     end
-    
-    if album[key] == nil then
-      album[key] = value
-    else
-      if album[key] != value then
-        puts key + " [" + value + "] does not match [" + album[key] + "."
-        exit(0)
-      end
-    end
-    
-    return album
+  end
+
+  return album
 end
 
 def setBitrate(album, value)
@@ -45,23 +45,23 @@ def setBitrate(album, value)
     if album[:bitrate]["MAX"] != nil
       max = album[:bitrate]["MAX"].to_i
     end
-    
+
     if album[:bitrate]["MIN"] != nil
       min = album[:bitrate]["MIN"].to_i
     end
-    
+
     if max < value.to_i
       max = value.to_i
     end
-    
+
     if min > value.to_i
       min = value.to_i
     end
-    
+
     album[:bitrate]["MAX"] = max.to_s
     album[:bitrate]["MIN"] = min.to_s
   end
-  
+
   return album
 end
 
@@ -70,56 +70,55 @@ def postprocess(album)
   if album[:bitrate]['FLAC'] == 'X'
     bitrate += 'FLAC'
   end
-  
+
   if album[:bitrate]["MAX"] != nil && album[:bitrate]["MIN"] != nil
     if bitrate != ""
       bitrate += ","
     end
-    
+
     if album[:bitrate]["MAX"] == album[:bitrate]["MIN"]
       bitrate += album[:bitrate]["MAX"]
     else
-      bitrate += album[:bitrate]["MIN"] + "-" + album[:bitrate]["MAX"] 
+      bitrate += album[:bitrate]["MIN"] + "-" + album[:bitrate]["MAX"]
     end
   end
-     
-  
+
   album.delete(:bitrate)
   album['bitrate'] = bitrate;
-  
+
   if album['gain'].end_with?(' dB')
     album['gain'] = album['gain'].slice(0, album['gain'].length() - 3)
   end
-  
+
   album["size"] = '%.2f' % ((album["size"]/1024.0)/1024.0)
   album["length"] = Time.at(album["length"]).gmtime.strftime('%R:%S')
-  
+
   return album
 end
 
 def processFile(path, album)
   puts "Processing: " + getLastPathElem(path)
-  
+
   TagLib::FileRef.open(path) do |file|
     tag = file.tag;
-    
+
     if tag == nil then
       puts "There's no tag on file."
       exit(0)
     end
-    
+
     album = setAlbumVar(album, "artist", tag.artist)
     album = setAlbumVar(album, "name", tag.album)
     album = setAlbumVar(album, "year", tag.year)
-    
+
     properties = file.audio_properties
-    album["length"] = album["length"] + properties.length 
-    
+    album["length"] = album["length"] + properties.length
+
     album["size"] = album["size"] + File.size(path)
-    
+
     return album
   end
-  
+
   puts "Cannot read file."
   exit(0)
 end
@@ -127,14 +126,13 @@ end
 def processMp3(path, album)
   gain = getMp3RG(path)
   album = setAlbumVar(album, "gain", gain)
-  
+
   TagLib::FileRef.open(path) do |file|
     prop = file.audio_properties
     album = setBitrate(album, prop.bitrate)
     puts prop.bitrate
     puts prop.length
   end
-    
   return album
 end
 
@@ -148,37 +146,37 @@ end
 def processDir(path)
   albumDir = getLastPathElem(path)
   puts "Processing Album: " + albumDir
-  
+
   album = dirName2Album(albumDir)
   album["size"] = 0
   album["length"] = 0
 
   Find.find(path) do |subdirPath|
-   if FileTest.directory?(subdirPath) 
-     next
-   end
- 
-   puts album
-  
-   filePath = subdirPath.gsub('/', '\\')
-   
-   fileFullName = getLastPathElem(filePath)
-   fileExt = getExt(fileFullName)
-   fileName = getFileName(fileFullName)
-   
-   if (fileExt == 'mp3' || fileExt == 'flac')
-     album = processFile(filePath, album)
+    if FileTest.directory?(subdirPath)
+      next
+    end
 
-     if (fileExt == 'mp3')
-       album = processMp3(filePath, album)
-     end
-  
-     if (fileExt == 'flac')
-       album = processFlac(filePath, album)
-     end
-   end
+    puts album
+
+    filePath = subdirPath.gsub('/', '\\')
+
+    fileFullName = getLastPathElem(filePath)
+    fileExt = getExt(fileFullName)
+    fileName = getFileName(fileFullName)
+
+    if (fileExt == 'mp3' || fileExt == 'flac')
+      album = processFile(filePath, album)
+
+      if (fileExt == 'mp3')
+        album = processMp3(filePath, album)
+      end
+
+      if (fileExt == 'flac')
+        album = processFlac(filePath, album)
+      end
+    end
   end
-  
+
   album = postprocess(album)
   puts album
 
@@ -186,12 +184,12 @@ def processDir(path)
 
   body = x.body
   update = false;
-  
+
   if body.start_with?('U.')
     update = true
     body = body.slice('U.'.length, body.length - 'U.'.length)
   end
-  
+
   if body.start_with?('row_id:')
     body = body.slice('row_id:'.length, body.length - 'row_id:'.length)
     puts "Album ID: " + body
@@ -201,7 +199,7 @@ def processDir(path)
       puts "Album created. ID=" + body
       uploadPictures(body)
     end
-    
+
     if ENV['BROWSER'] != nil
       exec("\"" + ENV['BROWSER'] + "\" \"http://vzasade.com/mdb/pages/main.php?row_id=" + body + "\"");
     end
@@ -210,7 +208,7 @@ def processDir(path)
   end
 
   exit(0)
-end  
+end
 
 ######################################################
 # Body
