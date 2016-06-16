e735c777a1c636e075c4ca31a49a17af2bc3f980
Artem Stemkovski <astemkov@gmail.com>
2016-06-16 01:27:43 -0700

    script for converting to 96/24

---
+++ hrc.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+
+$sox_path = "\"" + ENV['SOX_PATH'] + "\""
+
+def executeCommand(command)
+  if !system(command)
+    puts "Failed to execute command: " + command
+    exit(0)
+  end
+end
+
+def processFile(path)
+  puts("Processing: " + path)
+  pathWithoutExt = File.join(File.dirname(path), File.basename(path, File.extname(path)))
+  executeCommand($sox_path + " \"" + path + "\"  -r 96000 -b 24 \"" + pathWithoutExt + "_96.wav\"")
+end
+
+def processDir(path)
+  Dir.chdir(path)
+  Dir.glob('**/*.{ape,wv,flac,wav}').sort.each do |f|
+    processFile(path + "/" + f)
+  end
+end
+
+######################################################
+# Body
+######################################################
+
+if ARGV.length != 1
+  puts 'Incorrect number of arguments!'
+  puts 'USAGE: hrc.rb <path to album dir>'
+  exit -1
+end
+
+processDir(ARGV[0])
